2c5aa074d06f7f8e1aab4fe34399157cdd2bf793
Chantz-J <johnson.chantz@gmail.com>
2022-02-04 20:48:19 -0500

    Heroku push attempt 21

--- src/server.js
+++ src/server.js
@@ -28,12 +28,12 @@ if (!notionDatabaseId || !notionSecret) {
   throw Error("Must define NOTION_SECRET and NOTION_DATABASE_ID in env");
 }
 
- server.all('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin: *');
-  res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-   next();
- });
+//  server.all('*', (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin: *');
+//   res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+//    next();
+//  });
 
 
 server.get('/', (req, res) => {
